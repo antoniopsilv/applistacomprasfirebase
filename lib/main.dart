c07951101feb15ae2c383468d774a58cb50017ab
antoniopsilv <antoniop.silv@gmail.com>
2024-11-12 09:50:23 -0300

    initialize database fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:applistacomprasfirebase/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/listacompras.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MaterialApp(
       title: 'DM3 - Lista Compras Firebase',
